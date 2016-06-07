031e6835dbbc16cb237da706c1559a2ae9f74237
colinmarco <colinmarco1@gmail.com>
2016-06-07 09:45:47 +0200

    Debut 2eme journée

--- src/main/java/streaming/controller/JoueurController.java
+++ src/main/java/streaming/controller/JoueurController.java
@@ -5,10 +5,14 @@
  */
 package streaming.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import streaming.service.JoueurCrudService;
+import streaming.service.JoueurService;
+import streaming.service.TableJeuService;
 
 /**
  *
@@ -16,9 +20,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class JoueurController {
+    
+     
+    @Autowired
+    JoueurCrudService joueurcrud;
+    
+     @Autowired
+    JoueurService joueurservice;
+     
+     @Autowired
+    TableJeuService tableservice;
+     
     @RequestMapping(value = "/lancersort", method = RequestMethod.GET)
     public String sortGET(Model model){
         
+        model.addAttribute("listesort",tableservice.listeSort(joueurcrud.findAllByMarqueurMainTrue().get(0).getId()) );
         return "sort";
     }
     
---
+++ src/main/webapp/WEB-INF/views/sort.jsp
@@ -0,0 +1,34 @@
+<%-- 
+    Document   : _TEMPLATE
+    Created on : 24 mai 2016, 09:50:06
+    Author     : ajc
+--%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <c:import url="_HEAD.jsp"/>
+    </head>
+    <body>
+        <div class="titre">
+        <c:import url="_TITRE.jsp"/>
+        </div>
+        <div class="contenu"></div> 
+        
+        <div class="menu">
+        <c:import url="_MENU.jsp"/>
+        </div>
+        
+        <form:form method="post" modelAttribute="listesort" >
+            <form:select path="listesort" name="sort" size="1">
+            </form:select>
+        </form:form>
+        
+        <div class="pied">
+        <c:import url="_PIED.jsp"/>
+        </div>
+        
+    </body>
+</html>

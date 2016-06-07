6d24d567f51fa0da90d9666ed5bcd45ee6ef62f2
ttg13 <damien.laffort@gmail.com>
2016-06-07 11:01:54 +0200

    Commit ALLEEEZZZZZ2

--- src/main/java/streaming/controller/TableJeuController.java
+++ src/main/java/streaming/controller/TableJeuController.java
@@ -24,46 +24,55 @@ import streaming.service.TableJeuService;
  */
 @Controller
 public class TableJeuController {
-    
+
     @Autowired
     JoueurCrudService joueurcrud;
-    
-     @Autowired
+
+    @Autowired
     JoueurService joueurservice;
-     
-     @Autowired
-     TableJeuService tablejeuservice;
-    
-     @RequestMapping(value = "/homepage", method = RequestMethod.GET)
-    public String homepageGET(Model model){
-        
+
+    @Autowired
+    TableJeuService tablejeuservice;
+
+    @RequestMapping(value = "/homepage", method = RequestMethod.GET)
+    public String homepageGET(Model model) {
+
         return "homepage";
     }
-    
-     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String loginGET( Model model){
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginGET(Model model) {
         model.addAttribute("ajoutjoueur", new Joueur());
         return "login";
     }
-    
+
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String loginPOST(@ModelAttribute("ajoutjoueur") Joueur j, HttpSession session){
-        
-        
+    public String loginPOST(@ModelAttribute("ajoutjoueur") Joueur j, HttpSession session) {
+
         joueurservice.creerJoueur(j.getPseudo(), j.getTypeSorciere());
         session.setAttribute("joueur", j);
         return "redirect:/homepage";
     }
-    
-    
-    
+
     //ce post permet de démarrer la partie
     @RequestMapping(value = "/launch", method = RequestMethod.GET)
-    public String launchPOST(Model model, HttpSession session){
-        
+    public String launchPOST(Model model, HttpSession session) {
+
         tablejeuservice.commencerJeu();
         session.setAttribute("partielancee", true);
-        
-       return"redirect:/homepage"; 
+        if (!joueurcrud.findAllByMarqueurMainTrue().isEmpty()) {
+            Joueur jtest = joueurcrud.findAllByMarqueurMainTrue().get(0);
+            Joueur j = (Joueur) session.getAttribute("joueur");
+
+            if (j.equals(jtest)) {
+                session.setAttribute("main", true);
+            } else {
+                session.setAttribute("main", false);
+            }
+        } else {
+            session.setAttribute("main", false);
+        }
+        return "redirect:/homepage";
     }
+
 }
